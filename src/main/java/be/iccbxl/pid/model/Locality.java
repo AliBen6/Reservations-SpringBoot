80c7ebb654e313494629024a19072d5e2387e2a7
benko <belmin.vehapi@hotmail.com>
2021-05-26 16:51:27 +0200

    Pre migration

--- src/main/java/be/iccbxl/pid/model/Locality.java
+++ src/main/java/be/iccbxl/pid/model/Locality.java
@@ -1,9 +1,13 @@
 package be.iccbxl.pid.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
@@ -15,8 +19,11 @@ public class Locality {
 	private Long id;
 	private String postalCode;
 	private String locality;
-	
-	protected Locality() {	}
+
+	@OneToMany( targetEntity=Location.class, mappedBy="locality" )
+	private List<Location> locations = new ArrayList<>();
+
+	protected Locality() { }
 
 	public Locality(String postalCode, String locality) {
 		this.postalCode = postalCode;
@@ -26,26 +33,50 @@ public class Locality {
 	public Long getId() {
 		return id;
 	}
-	
+
 	public String getPostalCode() {
 		return postalCode;
 	}
-	
+
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
-	
+
 	public String getLocality() {
 		return locality;
 	}
-	
+
 	public void setLocality(String locality) {
 		this.locality = locality;
 	}
-	
+
+	public List<Location> getLocations() {
+		return locations;
+	}
+
+	public Locality addLocation(Location location) {
+		if(!this.locations.contains(location)) {
+			this.locations.add(location);
+			location.setLocality(this);
+		}
+
+		return this;
+	}
+
+	public Locality removeLocation(Location location) {
+		if(this.locations.contains(location)) {
+			this.locations.remove(location);
+			if(location.getLocality().equals(this)) {
+				location.setLocality(null);
+			}
+		}
+
+		return this;
+	}
+
 	@Override
 	public String toString() {
 		return "Locality [id=" + id + ", postalCode=" + postalCode + ", locality=" + locality + "]";
 	}
-	
+
 }
