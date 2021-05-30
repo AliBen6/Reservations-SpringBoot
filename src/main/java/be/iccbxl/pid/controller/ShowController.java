b24992d16718c1f630fb10ab5932e6f102b9d358
benko <belmin.vehapi@hotmail.com>
2021-05-30 15:24:38 +0200

    Update templates Show (added artists list)

--- src/main/java/be/iccbxl/pid/controller/ShowController.java
+++ src/main/java/be/iccbxl/pid/controller/ShowController.java
@@ -2,12 +2,18 @@ package be.iccbxl.pid.controller;
 
 import java.util.List;
 
+import be.iccbxl.pid.model.ArtistType;
+import be.iccbxl.pid.model.Type;
 import be.iccbxl.pid.service.ShowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import be.iccbxl.pid.model.Show;
 import be.iccbxl.pid.service.ShowService;
@@ -30,6 +36,7 @@ public class ShowController {
     @GetMapping("/shows/{id}")
     public String show(Model model, @PathVariable("id") String id) {
         Show show = service.get(id);
+
 
         model.addAttribute("show", show);
         model.addAttribute("title", "Fiche d'un spectacle");
